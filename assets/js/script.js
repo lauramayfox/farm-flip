a5ff143530668ec29f5c1609d0af3acb76f6511d
laura fox <laurafox91@gmail.com>
2024-02-22 18:09:29 +0000

    Add card flip function and check for match functions. Add data framework to HTML to link to the matching function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 //Variable values not being changed, constant
 const cards = document.querySelectorAll('.farm-card')
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+//Knowing which card flips first to make a match
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 
 function flipCard() {
-    console.log('test');
-    console.log(this)
+    this.classList.add('flip');
+
+//Adding a condition
+if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+}
+secondCard = this;
+    
+}
+
+function checkForMatch() {
+    let isMatch =
 }
