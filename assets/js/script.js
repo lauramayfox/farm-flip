e662794bec41bd01c55bde2f324091e63894c5ec
laura fox <laurafox91@gmail.com>
2024-02-22 18:43:49 +0000

    Update functions for disable, shuffle and reset board. Cards now stay flipped when matched, else flip back to original state.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,12 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 //Knowing which card flips first to make a match
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 //Declaring functions
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
 //Adding a condition
@@ -30,17 +33,31 @@ function checkForMatch() {
 }
 
 function disableCards () {
+firstCard.removeEventListener('flip');
+secondCard.removeEventListener('flip');
 
+resetBoard();
 }
 
 function unflipCards () {
+lockBoard = true;
 
+setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+//time before unmatched cards get flipped back over
+    resetBoard();
+  }, 1400);
 }
 
 function shuffle() {
-    
-}
+    cards.forEach(card => {
+      let randomPos = Math.floor(Math.random() * 12);
+      card.style.order = randomPos;
+    }) }
 
 function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 
 }
