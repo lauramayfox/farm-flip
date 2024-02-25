a2d5fe5e82dd5580f0ea10b375e6074645f03f79
laura fox <laurafox91@gmail.com>
2024-02-25 16:59:08 +0000

    Button event listeners and functions added for modal. HTML text update and padding fixed CSS modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,47 @@
-//JS Game board functions followed & adapted by Javascript Academy & Code Sketch tutorials - YouTube 
-
-//Variable values not being changed, constant
+//Variable values
 const cards = document.querySelectorAll('.farm-card')
-//Event listener for when card is clicked
-cards.forEach(card => card.addEventListener('click', flipCard)); shuffle();
 //Moves counter
 const moveContainer = document.querySelector(".moves");
+//Button - Rules
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+//Event listener for when card is clicked
+cards.forEach(card => card.addEventListener('click', flipCard)); shuffle();
+//Event Listener for Buttons open/close Web Dev Simplified tutorial
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+  //Closes modal when clicking outside the modal box
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+  
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal')
+      closeModal(modal)
+    })
+  })
+  
+  function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
 
 
 
