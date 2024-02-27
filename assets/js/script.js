7fa30d2db6af6b17b7d649ab60615dbe0115c689
laura fox <laurafox91@gmail.com>
2024-02-27 23:07:09 +0000

    Format HTML, CSS, JS files with online code formatting tool.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 //Variable values
-const cards = document.querySelectorAll('.farm-card')
-var totalNumberOfCards = cards.length
+const cards = document.querySelectorAll(".farm-card");
+var totalNumberOfCards = cards.length;
 //Moves counter
 const moveContainer = document.querySelector(".moves");
 //Rules Button
-const openModalButton = document.querySelectorAll('[data-modal-target]')
-const closeModalButton = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const openModalButton = document.querySelectorAll("[data-modal-target]");
+const closeModalButton = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
 //Knowing which card flips first to make a match
 let hasFlippedCard = false;
@@ -16,74 +16,76 @@ let moves = 0;
 let revealedCards = 0;
 
 //Event listener for when card is clicked
-cards.forEach(card => card.addEventListener('click', flipCard)); shuffle();
+cards.forEach((card) => card.addEventListener("click", flipCard));
+shuffle();
 
 //Event Listener for Buttons open/close Web Dev Simplified tutorial
-openModalButton.forEach(button => {
-    button.addEventListener('click', () => {
-      const modal = document.querySelector(button.dataset.modalTarget)
-      openModal(modal)
-    })
-  })
-  //Closes modal when clicking outside the modal box Web Dev Simplified tutorial 
-  overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-      closeModal(modal)
-    })
-  })
-  
-  closeModalButton.forEach(button => {
-    button.addEventListener('click', () => {
-      const modal = button.closest('.modal')
-      closeModal(modal)
-    })
-  })
-  
-  function openModal(modal) {
-    if (modal == null) {
-      return}
-    modal.classList.add('active')
-    overlay.classList.add('active')
-  }
-  
-  function closeModal(modal) {
-    if (modal == null) {
-      return}
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-  }
+openModalButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+//Closes modal when clicking outside the modal box Web Dev Simplified tutorial
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
 
+closeModalButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
 
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 
 //Score count feature Javascript Academy tutorial
 moves = 0;
 moveContainer.innerHtml = 0;
 
 function addMove() {
-moves++;
-moveContainer.innerHTML = moves;
+  moves++;
+  moveContainer.innerHTML = moves;
 }
 
 function flipCard() {
-    if (lockBoard) {
-      return}
-    if (this === firstCard) {
-      return}
-    this.classList.add('flip');
+  if (lockBoard) {
+    return;
+  }
+  if (this === firstCard) {
+    return;
+  }
+  this.classList.add("flip");
 
-//Adding a condition
-if (!hasFlippedCard) {
+  //Adding a condition
+  if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
 
     return;
+  }
+  //On second click
+  secondCard = this;
+  checkForMatch();
 }
-//On second click
-secondCard = this;
-    checkForMatch();
-}
-
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
@@ -99,32 +101,32 @@ function checkForMatch() {
   }
 }
 
-
 //Code Sketch Lab adaptation
 
-function disableCards () {
-firstCard.removeEventListener('click', flipCard);
-secondCard.removeEventListener('click', flipCard);
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
 
-resetBoard();
+  resetBoard();
 }
 
-function unflipCards () {
-lockBoard = true;
+function unflipCards() {
+  lockBoard = true;
 
-setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-//time before unmatched cards get flipped back over
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    //time before unmatched cards get flipped back over
     resetBoard();
   }, 1400);
 }
 
 function shuffle() {
-    cards.forEach(card => {
-      let randomPos = Math.floor(Math.random() * 12);
-      card.style.order = randomPos;
-    }) }
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+}
 
 function resetBoard() {
   hasFlippedCard = false;
@@ -134,25 +136,25 @@ function resetBoard() {
 }
 //Restart Game Button adaption of Javascript Academy tutorial
 function restart() {
-     setTimeout(() => {
+  setTimeout(() => {
     moves = 0;
     moveContainer.innerHTML = 0;
     flippedCard = false;
     hasFlippedCard = false;
     firstCard = null;
     secondCard = null;
-    cards.forEach(cardReset => cardReset.classList.remove('flip'));
+    cards.forEach((cardReset) => cardReset.classList.remove("flip"));
     shuffle();
-    cards.forEach(card => card.addEventListener('click', flipCard));
-}, 300);
+    cards.forEach((card) => card.addEventListener("click", flipCard));
+  }, 300);
 }
 
 //End game to trigger alert and reset cards
 function endGame() {
   setTimeout(() => {
-    cards.forEach(card => card.classList.remove('flip'));
+    cards.forEach((card) => card.classList.remove("flip"));
     resetBoard();
-    alert('Win!');
+    alert("Win!");
     restart();
-  }, 1500); 
+  }, 1500);
 }
